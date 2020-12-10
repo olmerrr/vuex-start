ea7e36e449b3969214267651c3fb38b95f57e089
Yaroslav <dombrovskiy91@gmail.com>
2020-12-10 12:13:31 +0200

    add vuex store

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <base-container title="Vuex1.1">
+    <h2>{{ $store.state.counter }}</h2>
+    <button>+</button>
+    <button>-</button>
+
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+
+export default {
+  components: {
+    BaseContainer,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
