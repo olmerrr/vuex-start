47447fa9928f707f8ecf94df4fd9b427ad250b3f
Yaroslav <dombrovskiy91@gmail.com>
2020-12-10 21:22:15 +0200

    action context

--- src/main.js
+++ src/main.js
@@ -28,7 +28,8 @@ const store = createStore({
       }, 2000)
     },
     increase(context,payload){
-        context.commit('increase', payload);
+      console.log(context);
+      context.commit('increase', payload);
     },
   },
   getters: {
